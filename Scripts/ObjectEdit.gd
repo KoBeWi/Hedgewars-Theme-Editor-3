f82e1bfd4a831a77a587c8e3718562efc5bb2c08
Tomasz Chabora <kobewi4e@gmail.com>
2018-10-04 00:20:52 +0200

    Added rectangle removing

--- Scripts/ObjectEdit.gd
+++ Scripts/ObjectEdit.gd
@@ -1,11 +1,13 @@
 extends Panel
 
+onready var image = $ObjectImage
+
 var object
 var zoom = 1 setget set_zoom
 var drag
 
 func _ready():
-	$ObjectImage.texture = Util.load_texture(str(HWTheme.path(), object.name, ".png"))
+	image.texture = Util.load_texture(str(HWTheme.path(), object.name, ".png"))
 	
 	connect("resized", self, "move_view")
 	$UI/Zoom.connect("value_changed", self, "set_zoom")
@@ -15,7 +17,10 @@ func _ready():
 	move_view()
 
 func _process(delta):
-	if drag: $ObjectImage.position = drag + get_viewport().get_mouse_position()
+	if drag: image.position = drag + get_viewport().get_mouse_position()
+	
+	for rect in object.buried: if rect.has_point(image.mouse()): image.selected_rects.append(rect)
+	for rect in object.visible: if rect.has_point(image.mouse()): image.selected_rects.append(rect)
 
 func _input(event):
 	if event is InputEventMouseButton:
@@ -25,15 +30,15 @@ func _input(event):
 			elif event.button_index == BUTTON_WHEEL_DOWN:
 				self.zoom = max(zoom - 0.5, 0)
 			elif event.button_index == BUTTON_MIDDLE:
-				drag = $ObjectImage.position - get_viewport().get_mouse_position()
+				drag = image.position - get_viewport().get_mouse_position()
 		elif event.button_index == BUTTON_MIDDLE:
 			drag = null
-		elif event.button_index == BUTTON_LEFT:
-			object.visible.append($ObjectImage.get_drawn_rectangle())
-			$ObjectImage.stop_rectangle()
-		elif event.button_index == BUTTON_RIGHT:
-			object.buried.append($ObjectImage.get_drawn_rectangle())
-			$ObjectImage.stop_rectangle()
+		elif event.button_index == BUTTON_LEFT and image.drawing != null:
+			object.visible.append(image.get_drawn_rectangle())
+			image.stop_rectangle()
+		elif event.button_index == BUTTON_RIGHT and image.drawing != null:
+			object.buried.append(image.get_drawn_rectangle())
+			image.stop_rectangle()
 		
 	if event is InputEventKey and event.is_pressed() and event.scancode == KEY_ESCAPE:
 		queue_free()
@@ -45,17 +50,17 @@ func _unhandled_input(event):
 	if event is InputEventMouseButton:
 		if event.is_pressed():
 			if event.button_index == BUTTON_LEFT:
-				$ObjectImage.start_rectangle(false)
+				image.start_rectangle(false)
 			elif event.button_index == BUTTON_RIGHT:
-				$ObjectImage.start_rectangle(true)
+				image.start_rectangle(true)
 
 func set_zoom(new_zoom):
 	zoom = new_zoom
-	$ObjectImage.scale = Vector2(new_zoom, new_zoom)
+	image.scale = Vector2(new_zoom, new_zoom)
 	
 	$UI/Zoom.value = new_zoom
 	$UI/Zoom/Label.text = str("x", zoom)
 	$UI/Zoom/Label.rect_position.y = $UI/Zoom.rect_size.y - zoom/20.0 * ($UI/Zoom.rect_size.y-16) - 20
 
 func move_view():
-	$ObjectImage.position = rect_position + rect_size/2
+	image.position = rect_position + rect_size/2
