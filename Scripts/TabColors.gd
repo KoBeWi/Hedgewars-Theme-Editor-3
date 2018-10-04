464fb45cc4a9c5b172d3446593d87ef716c99dc7
Tomasz Chabora <kobewi4e@gmail.com>
2018-10-04 20:37:02 +0200

    Added sprites, fixed colors, etc.

--- Scripts/TabColors.gd
+++ Scripts/TabColors.gd
@@ -7,15 +7,15 @@ func _ready():
 	Util.size_listeners.append(self)
 	HWTheme.connect("theme_loaded", self, "on_theme_loaded")
 	
-	$UpperWater/Color.connect("color_changed", self, "synchronize_water_alpha", [$LowerWater/Color])
-	$LowerWater/Color.connect("color_changed", self, "synchronize_water_alpha", [$UpperWater/Color])
-	$SDUpperWater/Color.connect("color_changed", self, "synchronize_water_alpha", [$SDLowerWater/Color])
-	$SDLowerWater/Color.connect("color_changed", self, "synchronize_water_alpha", [$SDUpperWater/Color])
+	$UpperWater/ColorContainer/Color.connect("color_changed", self, "synchronize_water_alpha", [$LowerWater/ColorContainer/Color])
+	$LowerWater/ColorContainer/Color.connect("color_changed", self, "synchronize_water_alpha", [$UpperWater/ColorContainer/Color])
+	$SDUpperWater/ColorContainer/Color.connect("color_changed", self, "synchronize_water_alpha", [$SDLowerWater/ColorContainer/Color])
+	$SDLowerWater/ColorContainer/Color.connect("color_changed", self, "synchronize_water_alpha", [$SDUpperWater/ColorContainer/Color])
 	
 	for i in get_child_count():
 		var picker = get_child(i)
 		picker.get_node("Header/OnOff").connect("toggled", HWTheme, "change_property", [str(colors[i], "_defined")])
-		picker.get_node("Color").connect("color_changed", HWTheme, "change_property", [colors[i]])
+		picker.get_node("ColorContainer/Color").connect("color_changed", HWTheme, "change_property", [colors[i]])
 
 func synchronize_water_alpha(new_color, twin):
 	twin.color.a = new_color.a
@@ -25,4 +25,4 @@ func on_theme_loaded():
 		var picker = get_child(i)
 		
 		picker.get_node("Header/OnOff").pressed = HWTheme.get(str(colors[i], "_defined"))
-		picker.get_node("Color").color = HWTheme.get(colors[i])
+		picker.get_node("ColorContainer/Color").color = HWTheme.get(colors[i])
--- Scripts/TabObjects.gd
+++ Scripts/TabObjects.gd
@@ -8,10 +8,31 @@ func _ready():
 func on_theme_loaded():
 	for panel in get_children(): panel.free()
 	
+	for spray in HWTheme.sprays.keys():
+		var panel = preload("res://Nodes/SprayPanel.tscn").instance()
+		panel.spray = spray
+		add_child(panel)
+	
 	for object in HWTheme.objects.values():
 		var panel  = preload("res://Nodes/ObjectPanel.tscn").instance()
 		panel.object = object
 		add_child(panel)
+
+func update_object_amount(amount, object):
+	HWTheme.objects[object].number = amount
+	HWTheme.apply_change()
+
+func update_object_on_water(value, object):
+	if !value:
+		HWTheme.objects[object].buried.erase(Rect2())
+	else:
+		HWTheme.objects[object].buried.insert(0, Rect2())
+	
+	HWTheme.apply_change()
+
+func update_spray_amount(amount, spray):
+	HWTheme.sprays[spray] = amount
+	HWTheme.apply_change()
 
 func edit_object(object):
 	Util.temp_editor = get_tree().current_scene
