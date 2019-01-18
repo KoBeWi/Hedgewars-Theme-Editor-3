16ff3f2ca1812b24413573324b5dc38c7f814e27
Tomasz Chabora <kobewi4e@gmail.com>
2019-01-19 00:17:41 +0100

    Added moving images between lists

--- Scripts/TabCustomization.gd
+++ Scripts/TabCustomization.gd
@@ -3,13 +3,25 @@ extends Control
 func _ready():
 	get_parent().name = tr("Customization")
 	HWTheme.connect("theme_loaded", self, "on_theme_loaded")
+	
+	$ItemLists/LeftMove/MoveLeft.connect("pressed", self, "move_item", [$ItemLists/Other/List, $ItemLists/Sprays/List])
+	$ItemLists/LeftMove/MoveRight.connect("pressed", self, "move_item", [$ItemLists/Sprays/List, $ItemLists/Other/List])
+	$ItemLists/RightMove/MoveLeft.connect("pressed", self, "move_item", [$ItemLists/Objects/List, $ItemLists/Other/List])
+	$ItemLists/RightMove/MoveRight.connect("pressed", self, "move_item", [$ItemLists/Other/List, $ItemLists/Objects/List])
 
 func on_theme_loaded():
-	$ItemLists/Sprays.clear()
-	$ItemLists/Objects.clear()
+	$ItemLists/Sprays/List.clear()
+	$ItemLists/Objects/List.clear()
 	
 	for s in HWTheme.sprays:
-		$ItemLists/Sprays.add_item(s)
+		$ItemLists/Sprays/List.add_item(s)
 	
 	for o in HWTheme.objects:
-		$ItemLists/Objects.add_item(o)
+		$ItemLists/Objects/List.add_item(o)
+
+func move_item(from, to):
+	if from.get_item_count() == 0 or from.get_selected_items().size() == 0: return
+	
+	var item = from.get_selected_items()[0]
+	to.add_item(from.get_item_text(item))
+	from.remove_item(item)
