53af69cfc08400ab55e44ec19f0bbf89f64d9c9b
Tomasz Chabora <kobewi4e@gmail.com>
2019-02-26 16:47:32 +0100

    All customization panels now appear

--- Scripts/TabCustomization.gd
+++ Scripts/TabCustomization.gd
@@ -24,9 +24,9 @@ func on_theme_loaded():
 	customization = add_customization("Icon")
 	customization.add_info("Themes without icons don't appear on in-game theme list")
 	customization.add_image("icon.png")
-	customization.add_image_info("Size: 33x32")
+	customization.add_image_info("Size: %s", ["33x32"])
 	customization.add_image("icon@2x.png")
-	customization.add_image_info("Size: 65x64")
+	customization.add_image_info("Size: %s", ["65x64"])
 	
 	customization = add_customization("Land texture")
 	customization.add_info("Texture drawn as map's terrain")
@@ -43,8 +43,162 @@ func on_theme_loaded():
 	customization.add_info("Painted arround terrain")
 	customization.add_info("This image is required")
 	customization.add_image("Border.png")
-	customization.add_image_info("Size: 128x32")
-	customization.add_image_info("Two halves, top and bottom 16 pix each")
+	customization.add_image_info("Size: %s", ["128x32"])
+	customization.add_image_info("Two halves, top and bottom %s pix each", ["16"])
+	
+	customization = add_customization("Girder")
+	customization.add_info("Land bridges")
+	customization.add_info("If not provided, a default image is used")
+	customization.add_image("Girder.png")
+	customization.add_image_info("Any size")
+	
+	customization = add_customization("Chunk")
+	customization.add_info("Particles that fall when land is destroyed")
+	customization.add_info("If not provided, no particles will appear") #TODO: no %s will appear?
+	customization.add_image("Chunk.png")
+	customization.add_image_info("Size: %s", ["64x64"])
+	customization.add_image_info("Each particle is %s square", ["32x32"])
+	
+	customization = add_customization("Dust")
+	customization.add_info("Particles that fall when something hits ground or mudball flies etc.")
+	customization.add_info("If not provided, a default image is used")
+	customization.add_image("Dust.png")
+	customization.add_image_info("Size: %s", ["22x176"])
+	customization.add_image_info("Each particle is %s square", ["22x22"])
+	
+	customization = add_customization("Water")
+	customization.add_info("Texture of water waves")
+	customization.add_info("If not provided, a default image is used")
+	customization.add_info("SD version is used during Sudden Death")
+	customization.add_image("BlueWater.png")
+	customization.add_image_info("Any size")
+	customization.add_image_info("%s is preferred", ["128x48"])
+	customization.add_image("SDWater.png")
+	customization.add_image_info("Any size")
+	customization.add_image_info("%s is preferred", ["128x48"])
+	
+	customization = add_customization("Splash and droplets")
+	customization.add_info("Used when something hits water")
+	customization.add_info("If not provided, a default image is used")
+	customization.add_info("SD version is used during Sudden Death")
+	customization.add_image("Splash.png")
+	customization.add_image_info("Size: %s frames %s pix each", ["20", "80x50"])
+	customization.add_image_info("Lined vertically in two columns")
+	customization.add_image("Droplet.png")
+	customization.add_image_info("Size: %s images %s pix each, lined vertically", ["4", "16x16"])
+	customization.add_image_info("Lined vertically")
+	customization.add_image("SDSplash.png")
+	customization.add_image_info("Size: %s frames %s pix each", ["20", "80x50"])
+	customization.add_image_info("Lined vertically in two columns")
+	customization.add_image("SDDroplet.png")
+	customization.add_image_info("Size: %s images %s pix each, lined vertically", ["4", "16x16"])
+	customization.add_image_info("Lined vertically")
+	
+	customization = add_customization("Clouds")
+	customization.add_info("Texture for clouds that appear on top of the map")
+	customization.add_info("If not provided, a default image is used")
+	customization.add_info("SD version is used during Sudden Death")
+	customization.add_image("Clouds.png")
+	customization.add_image_info("Size: %s for each cloud", ["256x128"])
+	customization.add_image_info("Lined vertically")
+	customization.add_image_info("Any number")
+	customization.add_image("SDClouds.png")
+	customization.add_image_info("Size: %s for each cloud", ["256x128"])
+	customization.add_image_info("Lined vertically")
+	customization.add_image_info("Any number")
+	
+	customization = add_customization("Flakes")
+	customization.add_info("Texture for flakes that float in background")
+	customization.add_info("If not provided, no flakes will appear") #TODO: verify
+	customization.add_info("SD version is used during Sudden Death")
+	customization.add_image("Flake.png")
+	customization.add_image_info("Size: %s for each flake", ["64x64"])
+	customization.add_image_info("Lined vertically")
+	customization.add_image("SDFlake.png")
+	customization.add_image_info("Size: %s for each flake", ["64x64"])
+	customization.add_image_info("Lined vertically")
+	
+	customization = add_customization("Mirrored clouds/flakes")
+	customization.add_info("Will replace their equivalents when wind is blowing left")
+	customization.add_info("If not provided, flakes/clouds will always appear in one direction")
+	customization.add_image("CloudsL.png")
+	customization.add_image_info("Size: %s for each cloud", ["256x128"])
+	customization.add_image_info("Lined vertically")
+	customization.add_image_info("Any number")
+	customization.add_image("SDCloudsL.png")
+	customization.add_image_info("Size: %s for each cloud", ["256x128"])
+	customization.add_image_info("Lined vertically")
+	customization.add_image_info("Any number")
+	customization.add_image("FlakeL.png")
+	customization.add_image_info("Size: %s for each flake", ["64x64"])
+	customization.add_image_info("Lined vertically")
+	customization.add_image("SDFlakeL.png")
+	customization.add_image_info("Size: %s for each flake", ["64x64"])
+	customization.add_image_info("Lined vertically")
+	
+	customization = add_customization("Snow")
+	customization.add_info("Texture for snow that accumulates on map when snow is enabled")
+	customization.add_info("If not provided, a default image is used")
+	customization.add_image("Snow.png")
+	customization.add_image_info("Any size") #TODO: verify
+	
+	customization = add_customization("Sky")
+	customization.add_info("Map's background texture")
+	customization.add_info("If not provided, no sky will appear") #TODO: no %s will appear?
+	customization.add_image("Sky.png")
+	customization.add_image_info("Any size")
+	#TODO: add preferred size?
+	
+	customization = add_customization("Left/right sky")
+	customization.add_info("If SkyL exists, Sky will be centered and SkyL will be looped on both sides")
+	customization.add_info("If both SkyL and SkyR exist, Sky will be centered, SkyL will be looped on left side and SkyR on right side")
+	customization.add_image("SkyL.png")
+	customization.add_image_info("Any size")
+	customization.add_image_info("Height preferrably the same as %s", ["Sky.png"])
+	customization.add_image("SkyR.png")
+	customization.add_image_info("Any size")
+	customization.add_image_info("Height preferrably the same as %s", ["Sky.png"])
+	
+	customization = add_customization("Horizont")
+	customization.add_info("Background layer between sky and terrain")
+	customization.add_info("If not provided, no horizont will appear") #TODO: no %s will appear?
+	customization.add_image("horizont.png")
+	customization.add_image_info("Any size")
+	
+	customization = add_customization("Left/right horizont")
+	customization.add_info("Same as SkyL/SkyR, but for horizont")
+	customization.add_image("horizontL.png")
+	customization.add_image_info("Any size")
+	customization.add_image_info("Height preferrably the same as %s", ["horizont.png"])
+	customization.add_image("horizontR.png")
+	customization.add_image_info("Any size")
+	customization.add_image_info("Height preferrably the same as %s", ["horizont.png"])
+	
+	customization = add_customization("Girder tool")
+	customization.add_info("Used as a replacement for girder tool")
+	customization.add_info("Any shape is allowed and will have proper collisions")
+	customization.add_info("Use template if you want proper layout")
+	customization.add_info("If not provided, a default image is used")
+	customization.add_image("amGirder.png")
+	customization.add_image_info("%s grid images", ["3x3"])
+	customization.add_image_info("Each image is %s pix", ["160x160"])
+	
+	customization = add_customization("Rubber tool")
+	customization.add_info("Used as a replacement for rubber tool")
+	customization.add_info("Any shape is allowed and will have proper collisions")
+	customization.add_info("Use template if you want proper layout")
+	customization.add_info("If not provided, a default image is used")
+	customization.add_image("amRubber.png")
+	customization.add_image_info("%s grid images", ["2x2"])
+	customization.add_image_info("Each image is %s pix", ["160x160"])
+	
+	customization = add_customization("Mudball weapon")
+	customization.add_info("Used as a replacement for mudball weapon")
+	customization.add_info("If not provided, a default image is used")
+	customization.add_image("Snowball.png")
+	customization.add_image_info("Size: %s", ["16x16"])
+	customization.add_image("amSnowball.png")
+	customization.add_image_info("Size: %s", ["128x128"])
 
 func move_item(from, to):
 	if from.get_item_count() == 0 or from.get_selected_items().size() == 0: return
@@ -56,5 +210,13 @@ func move_item(from, to):
 func add_customization(cname):
 	var custom = preload("res://Nodes/CustomizationPanel.tscn").instance()
 	add_child(custom)
-	custom.get_node("Container/GroupName").text = cname
+	custom.get_node("Container/GroupName").text = tr(cname)
 	return custom
+
+const TEMPLATE_LIST = {
+	"LandText.png": ""
+}
+
+func fetch_template(iname):
+	var dir = Directory.new()
+	dir.copy(TEMPLATE_LIST[iname], HWTheme.path() + iname)
