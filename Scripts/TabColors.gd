40b81f84c32843fd7288cfb4f729ce764ccc5630
Tomasz Chabora <kobewi4e@gmail.com>
2019-05-15 03:09:53 +0200

    Color bugfix

--- Scripts/TabColors.gd
+++ Scripts/TabColors.gd
@@ -6,13 +6,13 @@ func _ready():
 	get_parent().name = tr("Colors")
 	HWTheme.connect("theme_loaded", self, "on_theme_loaded")
 	
-	$UpperWater/ColorContainer/Color.connect("color_changed", self, "synchronize_water_alpha", [$LowerWater/ColorContainer/Color])
-	$LowerWater/ColorContainer/Color.connect("color_changed", self, "synchronize_water_alpha", [$UpperWater/ColorContainer/Color])
-	$SDUpperWater/ColorContainer/Color.connect("color_changed", self, "synchronize_water_alpha", [$SDLowerWater/ColorContainer/Color])
-	$SDLowerWater/ColorContainer/Color.connect("color_changed", self, "synchronize_water_alpha", [$SDUpperWater/ColorContainer/Color])
+	$PickerContainer/UpperWater/ColorContainer/Color.connect("color_changed", self, "synchronize_water_alpha", [$PickerContainer/LowerWater/ColorContainer/Color])
+	$PickerContainer/LowerWater/ColorContainer/Color.connect("color_changed", self, "synchronize_water_alpha", [$PickerContainer/UpperWater/ColorContainer/Color])
+	$PickerContainer/SDUpperWater/ColorContainer/Color.connect("color_changed", self, "synchronize_water_alpha", [$PickerContainer/SDLowerWater/ColorContainer/Color])
+	$PickerContainer/SDLowerWater/ColorContainer/Color.connect("color_changed", self, "synchronize_water_alpha", [$PickerContainer/SDUpperWater/ColorContainer/Color])
 	
-	for i in get_child_count():
-		var picker = get_child(i)
+	for i in $PickerContainer.get_child_count():
+		var picker = $PickerContainer.get_child(i)
 		picker.get_node("Header/OnOff").connect("toggled", HWTheme, "change_property", [str(colors[i], "_defined")])
 		picker.get_node("ColorContainer/Color").connect("color_changed", HWTheme, "change_property", [colors[i]])
 
@@ -20,8 +20,8 @@ func synchronize_water_alpha(new_color, twin):
 	twin.color.a = new_color.a
 
 func on_theme_loaded():
-	for i in get_child_count():
-		var picker = get_child(i)
+	for i in $PickerContainer.get_child_count():
+		var picker = $PickerContainer.get_child(i)
 		
 		picker.get_node("Header/OnOff").pressed = HWTheme.get(str(colors[i], "_defined"))
 		picker.get_node("ColorContainer/Color").color = HWTheme.get(colors[i])
