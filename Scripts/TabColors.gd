877456cb9fb3a30119c67df04e87a8afff828ac4
Tomasz Chabora <kobewi4e@gmail.com>
2020-09-02 14:31:54 +0200

    Changed Colors tab to use ColorPickerButton

--- Scripts/TabColors.gd
+++ Scripts/TabColors.gd
@@ -6,15 +6,15 @@ func _ready():
 	get_parent().name = tr("Colors")
 	HWTheme.connect("theme_loaded", self, "on_theme_loaded")
 	
-	$PickerContainer/UpperWater/ColorContainer/Color.connect("color_changed", self, "synchronize_water_alpha", [$PickerContainer/LowerWater/ColorContainer/Color])
-	$PickerContainer/LowerWater/ColorContainer/Color.connect("color_changed", self, "synchronize_water_alpha", [$PickerContainer/UpperWater/ColorContainer/Color])
-	$PickerContainer/SDUpperWater/ColorContainer/Color.connect("color_changed", self, "synchronize_water_alpha", [$PickerContainer/SDLowerWater/ColorContainer/Color])
-	$PickerContainer/SDLowerWater/ColorContainer/Color.connect("color_changed", self, "synchronize_water_alpha", [$PickerContainer/SDUpperWater/ColorContainer/Color])
+	$PickerContainer/UpperWater/ColorPickerButton.connect("color_changed", self, "synchronize_water_alpha", [$PickerContainer/LowerWater/ColorPickerButton])
+	$PickerContainer/LowerWater/ColorPickerButton.connect("color_changed", self, "synchronize_water_alpha", [$PickerContainer/UpperWater/ColorPickerButton])
+	$PickerContainer/SDUpperWater/ColorPickerButton.connect("color_changed", self, "synchronize_water_alpha", [$PickerContainer/SDLowerWater/ColorPickerButton])
+	$PickerContainer/SDLowerWater/ColorPickerButton.connect("color_changed", self, "synchronize_water_alpha", [$PickerContainer/SDUpperWater/ColorPickerButton])
 	
 	for i in $PickerContainer.get_child_count():
 		var picker = $PickerContainer.get_child(i)
 		picker.get_node("OnOff").connect("toggled", HWTheme, "change_property", [str(colors[i], "_defined")])
-		picker.get_node("ColorContainer/Color").connect("color_changed", HWTheme, "change_property", [colors[i]])
+		picker.get_node("ColorPickerButton").connect("color_changed", HWTheme, "change_property", [colors[i]])
 
 func synchronize_water_alpha(new_color, twin):
 	twin.color.a = new_color.a
@@ -24,4 +24,4 @@ func on_theme_loaded():
 		var picker = $PickerContainer.get_child(i)
 		
 		picker.get_node("OnOff").pressed = HWTheme.get(str(colors[i], "_defined"))
-		picker.get_node("ColorContainer/Color").color = HWTheme.get(colors[i])
+		picker.get_node("ColorPickerButton").color = HWTheme.get(colors[i])
